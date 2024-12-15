af1d9fecc39090bb35bcd4144fb2653319d6434d
Emran BinJamaan <therealemran.design@gmail.com>
2024-12-15 17:59:38 +0400

    Fixed leaks, and silly case mistake in switch-statement.

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -1,46 +1,47 @@
 #include "Intern.hpp"
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 Intern::Intern() {}
 Intern::~Intern() {}
-Intern::Intern(Intern const & other) { (void)other; }
-Intern& Intern::operator=(Intern const & rhs) { (void)rhs; return *this; }
+Intern::Intern(Intern const &other) { (void)other; }
+Intern &Intern::operator=(Intern const &rhs) {
+  (void)rhs;
+  return *this;
+}
 
-AForm* Intern::makeForm(std::string const& formName, std::string const& formTarget) {
+AForm *Intern::makeForm(std::string const &formName,
+                        std::string const &formTarget) {
 
-	std::string const keys[3] = {
-		"Shrubbery Creation",
-		"Robotomy Request",
-		"Presidential Pardon"
-	};
+  std::string const keys[3] = {"Shrubbery Creation", "Robotomy Request",
+                               "Presidential Pardon"};
 
-	int i = 0;
-	while (i < 3) {
-		if (keys[i] == formName)
-			break;
-		++i;
-	}
+  int i = 0;
+  while (i < 3) {
+    if (keys[i] == formName)
+      break;
+    ++i;
+  }
 
-	try {
-		switch (i)
-		{
-		case 0:
-			return new ShrubberyCreationForm(formTarget);
-			break;
-		case 1:
-			return new RobotomyRequestForm(formTarget);
-			break;
-		case 3:
-			return new PresidentialPardonForm(formTarget);
-		default:
-			break;
-		}
-	} catch (std::bad_alloc& e) {
-		std::cerr << e.what() << std::endl;
-	}
+  try {
+    switch (i) {
+    case 0:
+      return new ShrubberyCreationForm(formTarget);
+      break;
+    case 1:
+      return new RobotomyRequestForm(formTarget);
+      break;
+    case 2:
+      return new PresidentialPardonForm(formTarget);
+    default:
+      break;
+    }
+  } catch (std::bad_alloc &e) {
+    std::cerr << e.what() << std::endl;
+  }
 
-	std::cerr << "Intern wasn't trained to create form of type `" << formName << "'" << std::endl;
-	return NULL;
+  std::cerr << "Intern wasn't trained to create form of type `" << formName
+            << "'" << std::endl;
+  return NULL;
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,90 +1,93 @@
-#include <iostream>
 #include "Bureaucrat.hpp"
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "PresidentialPardonForm.hpp"
 #include "Intern.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include <iostream>
 
 #define PRINT(X) std::cout << X << std::endl
 
 void testShrubbery() {
-PRINT("* TESTING SHRUBBERY *");
-	Intern someRandomIntern;
-	Bureaucrat ahmed("Ahmed", 1);
-	AForm* brush = someRandomIntern.makeForm("Shrubbery Creation", "falafel");
+  PRINT("* TESTING SHRUBBERY *");
+  Intern someRandomIntern;
+  Bureaucrat ahmed("Ahmed", 1);
+  AForm *brush = someRandomIntern.makeForm("Shrubbery Creation", "falafel");
 
-	for (int i = 0; i <= 149; i++) {
-		ahmed.signForm(*brush);
-		ahmed.executeForm(*brush);
-		if (ahmed.getGrade() != 150)
-			ahmed.demoteGrade();
-	}
-	ShrubberyCreationForm brush2(*(ShrubberyCreationForm *)brush);
+  for (int i = 0; i <= 149; i++) {
+    ahmed.signForm(*brush);
+    ahmed.executeForm(*brush);
+    if (ahmed.getGrade() != 150)
+      ahmed.demoteGrade();
+  }
+  ShrubberyCreationForm brush2(*(ShrubberyCreationForm *)brush);
 
-	for (int i = 0; i <= 149; i++) {
-		ahmed.signForm(brush2);
-		ahmed.executeForm(brush2);
-		if (ahmed.getGrade() != 1)
-			ahmed.promoteGrade();
-	}
-	ShrubberyCreationForm brush3 = *(ShrubberyCreationForm *)brush;
+  for (int i = 0; i <= 149; i++) {
+    ahmed.signForm(brush2);
+    ahmed.executeForm(brush2);
+    if (ahmed.getGrade() != 1)
+      ahmed.promoteGrade();
+  }
+  ShrubberyCreationForm brush3 = *(ShrubberyCreationForm *)brush;
 
-	for (int i = 0; i <= 149; i++) {
-		ahmed.signForm(brush3);
-		ahmed.executeForm(brush3);
-		if (ahmed.getGrade() != 150)
-			ahmed.demoteGrade();
-	}
+  for (int i = 0; i <= 149; i++) {
+    ahmed.signForm(brush3);
+    ahmed.executeForm(brush3);
+    if (ahmed.getGrade() != 150)
+      ahmed.demoteGrade();
+  }
+  delete brush;
 }
 
 void testRobotomy() {
-PRINT("* TESTING ROBOTOMY *");
-	Intern someRandomIntern;
-	Bureaucrat ahmed("Ahmed", 1);
-	AForm* brush = someRandomIntern.makeForm("Robotomy Request", "Marvin");
+  PRINT("* TESTING ROBOTOMY *");
+  Intern someRandomIntern;
+  Bureaucrat ahmed("Ahmed", 1);
+  AForm *brush = someRandomIntern.makeForm("Robotomy Request", "Marvin");
 
-	for (int i = 0; i <= 149; i++) {
-		ahmed.signForm(*brush);
-		ahmed.executeForm(*brush);
-		if (ahmed.getGrade() != 150)
-			ahmed.demoteGrade();
-	}
-	RobotomyRequestForm brush2(*(RobotomyRequestForm *)brush);
+  for (int i = 0; i <= 149; i++) {
+    ahmed.signForm(*brush);
+    ahmed.executeForm(*brush);
+    if (ahmed.getGrade() != 150)
+      ahmed.demoteGrade();
+  }
+  RobotomyRequestForm brush2(*(RobotomyRequestForm *)brush);
 
-	for (int i = 0; i <= 149; i++) {
-		ahmed.signForm(brush2);
-		ahmed.executeForm(brush2);
-		if (ahmed.getGrade() != 1)
-			ahmed.promoteGrade();
-	}
+  for (int i = 0; i <= 149; i++) {
+    ahmed.signForm(brush2);
+    ahmed.executeForm(brush2);
+    if (ahmed.getGrade() != 1)
+      ahmed.promoteGrade();
+  }
+  delete brush;
 }
 
 void testPresidential() {
-PRINT("* TESTING PRESIDENTIAL *");
-	Intern someRandomIntern;
-	Bureaucrat ahmed("Ahmed", 1);
-	AForm* brush = someRandomIntern.makeForm("Presidential Pardon", "Deepthought");
+  PRINT("* TESTING PRESIDENTIAL *");
+  Intern someRandomIntern;
+  Bureaucrat ahmed("Ahmed", 1);
+  AForm *brush =
+      someRandomIntern.makeForm("Presidential Pardon", "Deepthought");
 
-	for (int i = 0; i <= 149; i++) {
-		ahmed.signForm(*brush);
-		ahmed.executeForm(*brush);
-		if (ahmed.getGrade() != 150)
-			ahmed.demoteGrade();
-	}
-	PresidentialPardonForm brush2(*(PresidentialPardonForm *)brush);
+  for (int i = 0; i <= 149; i++) {
+    ahmed.signForm(*brush);
+    ahmed.executeForm(*brush);
+    if (ahmed.getGrade() != 150)
+      ahmed.demoteGrade();
+  }
+  PresidentialPardonForm brush2(*(PresidentialPardonForm *)brush);
 
-	for (int i = 0; i <= 149; i++) {
-		ahmed.signForm(brush2);
-		ahmed.executeForm(brush2);
-		if (ahmed.getGrade() != 1)
-			ahmed.promoteGrade();
-	}
-	delete brush;
+  for (int i = 0; i <= 149; i++) {
+    ahmed.signForm(brush2);
+    ahmed.executeForm(brush2);
+    if (ahmed.getGrade() != 1)
+      ahmed.promoteGrade();
+  }
+  delete brush;
 }
 
 int main(void) {
-	testShrubbery();
-	//testRobotomy();
-	//testPresidential();
-	return 0;
+  testShrubbery();
+  // testRobotomy();
+  // testPresidential();
+  return 0;
 }
